1a6fb4d55f013dc7da11b46df4c8e32ea82c2dc9
levdev0 <bsze3lajlev@vasvari.org>
2024-09-23 16:38:00 +0200

    csúszka

--- effektek/jquery.js
+++ effektek/jquery.js
@@ -1,5 +1,11 @@
+const { speed } = require("jquery");
+
 $(document).ready(function(){});
 $(function(){
+    let r = 128;
+    let g = 128;
+    let b = 128;
+
     $(".piros").on("click", function(){
         $(this).hide(1000);
     });
@@ -11,4 +17,29 @@ $(function(){
     $("button").click(function(){
         $("div").show(1000);
     });
+
+    $(".sarga").click(function(){
+        $(this).slideUp(1000).slideDown(1000);
+    });
+
+    $(".piros").click(function(){
+        $(this).slideUp(1000).slideDown(1000);
+    });
+
+    $("[type=range]").change(function(){
+        console.log($("[name=red]").val());
+        let element = $(this);
+        switch (element.attr("name")) {
+            case "red":
+                r = element.val();
+            case "green":
+                g = element.val();
+            case "blue":
+                b = element.val();
+        }
+        $("span").text(`rgb(${r}, ${g}, ${b})`)
+    
+    
+    });
+    
 });
